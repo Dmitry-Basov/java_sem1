46c45560f1fc6c2448c7a86e836f4c76d25d8dd8
Dmitriy Basov <dmitrybasov_787788@mail.ru>
2024-12-12 16:13:23 +0300

    Task1

---
+++ Main.java
@@ -0,0 +1,44 @@
+// import java.util.Scanner;
+
+// public class Main{
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.println("Введите ваше имя: ");
+//         String name = scanner.nextLine();
+//         System.out.println("Привет, " + name + "!");
+//         scanner.close();
+//     }
+
+// }
+
+
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите ваше имя: ");
+        String s = scanner.next();
+
+        LocalDateTime dateTime = LocalDateTime.now();
+        int i = dateTime.getHour();
+
+        String temp = "";
+        if(i >= 5 && i < 12){
+            temp = "Доброе утро";
+        }
+        if(i >= 12 && i < 18){
+            temp = "Добрый день";
+        }
+        if(i >= 18 && i < 23  ){
+            temp = "Добрый вечер";
+        }
+        if(i<5 || i==23){
+            temp = "Доброй ночи";
+        }
+
+        System.out.println(temp + "," + s);
+    }
+
+}
